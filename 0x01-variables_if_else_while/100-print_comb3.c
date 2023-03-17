be5c8d1d0a9b1cec2c3515b47854e054840ad012
sounia <sounia@gmail.com>
2023-03-17 13:03:18 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,33 @@
 /*
- * File: 9-print_comb.c
+ * File: 10-print_comb2.c
  * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int num;
+	int digit1, digit2;
 
-	for (num = 0; num <= 9; num++)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
+		for (digit2 = 0; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
