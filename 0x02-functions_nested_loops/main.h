b768e95d808a4b6d18ce3f15aa235616b3b201a9
sounia <sounia@gmail.com>
2023-03-21 10:46:06 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,24 @@
-#include "unistd.h"
-/**
- * main - Entry point
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
  *
- * Return: Always 0 (Success)
+ * Desc: Header file containing declartions for all function
  */
-int main(void)
-{
-	char *sh = "_putchar";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_teble(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
 
-	return (0);
-}
+#endif
