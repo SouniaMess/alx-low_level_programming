ec71a3eb2fdca53a816039b7adf34e6e2131e69a
sounia <sounia@gmail.com>
2023-03-21 09:35:14 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
