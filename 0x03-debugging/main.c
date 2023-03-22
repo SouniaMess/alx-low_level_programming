e9345842ca905e82c588b5ed6500cbf25463b4fe
sounia <sounia@gmail.com>
2023-03-22 14:46:11 -0700

    2-largest_number.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,32 +1,7 @@
 
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
-
-int main(void)
-{
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-/*
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
-}
-
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
