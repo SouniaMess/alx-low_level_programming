16c295bb57f9aff67a77c513b9ff5e8bc686100e
sounia <sounia@gmail.com>
2023-03-22 14:43:07 -0700

    2-largest_number.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,3 +1,7 @@
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdio.h>
 
 /**
@@ -22,3 +26,10 @@ int main(void)
 
 	return (0);
 }
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* HOLBERTON_H */
