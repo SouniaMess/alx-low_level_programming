89928e5cf749544259747b6eff13b61fbf39b838
sounia <sounia@gmail.com>
2023-04-11 13:16:53 -0700

    advanced task doneé

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
 * *argstostr - concatenates all the arguments of the program
 * @ac: number of arguments
@@ -15,21 +14,16 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
 		len++;
 	}
-
 	str = malloc(sizeof(char) * (len + 1));
-
 	if (str == NULL)
 		return (NULL);
-
 	k = 0;
-
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -40,6 +34,5 @@ char *argstostr(int ac, char **av)
 		str[k] = '\n';
 		k++;
 	}
-	
 	return (str);
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
@@ -10,10 +9,8 @@
 int count_word(char *s)
 {
 	int flag, c, w;
-
 	flag = 0;
 	w = 0;
-
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
@@ -24,7 +21,6 @@ flag = 1;
 			w++;
 		}
 	}
-
 	return (w);
 }
 /**
@@ -38,17 +34,14 @@ char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
-
 	while (*(str + len))
 		len++;
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
-
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-
 	for (i = 0; i <= len; i++)
 	{
 if (str[i] == ' ' || str[i] == '\0')
@@ -59,7 +52,6 @@ if (str[i] == ' ' || str[i] == '\0')
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
-
 				while (start < end)
 				*tmp++ = str[start++];
 				*tmp = '\0';
@@ -71,8 +63,6 @@ if (str[i] == ' ' || str[i] == '\0')
 		else if (c++ == 0)
 			start = i;
 	}
-
 	matrix[k] = NULL;
-
 	return (matrix);
 }
