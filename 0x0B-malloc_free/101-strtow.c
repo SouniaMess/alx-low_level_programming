8519dbfb6baf097d776fcac145c42df2e1bbffb6
sounia <sounia@gmail.com>
2023-04-11 17:32:27 -0700

    101 modifie

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,18 +9,21 @@
 int count_word(char *s)
 {
 	int flag, c, w;
+
 	flag = 0;
 	w = 0;
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
-flag = 1;
+			flag = 1;
 			w++;
 		}
 	}
+
 	return (w);
 }
 /**
@@ -32,19 +35,22 @@ flag = 1;
  */
 char **strtow(char *str)
 {
-	char **matrix, *tmp;
+char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
+
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
+
 	for (i = 0; i <= len; i++)
 	{
-if (str[i] == ' ' || str[i] == '\0')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
 			if (c)
 			{
@@ -53,7 +59,7 @@ if (str[i] == ' ' || str[i] == '\0')
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
-				*tmp++ = str[start++];
+					*tmp++ = str[start++];
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
@@ -63,6 +69,8 @@ if (str[i] == ' ' || str[i] == '\0')
 		else if (c++ == 0)
 			start = i;
 	}
+
 	matrix[k] = NULL;
+
 	return (matrix);
 }
